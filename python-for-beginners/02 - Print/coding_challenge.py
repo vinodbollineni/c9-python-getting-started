465781c438dca2088a9fc40389f6027883a7f31d
vinodbollineni <161277481+vinodbollineni@users.noreply.github.com>
2024-04-15 10:33:26 +0530

    Update coding_challenge.py

--- python-for-beginners/02 - Print/coding_challenge.py
+++ python-for-beginners/02 - Print/coding_challenge.py
@@ -5,11 +5,10 @@
 print('Why won't this line of code print')
 
 # print the message
-prnit('This line fails too!')
+print('This line fails too!')
 
 # print the message
-print "I think I know how to fix this one"
+print("I think I know how to fix this one")
 
 # print the name entered by the user
 input('Please tell me your name: ')
-print(name)
